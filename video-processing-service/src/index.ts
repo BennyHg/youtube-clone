68ff838fc7926ab8ba969c54609b41f93253c0a8
BennyHg <bhuang997@gmail.com>
2024-03-06 18:18:40 -0800

    video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,36 @@
 import express from 'express';
 import Ffmpeg from 'fluent-ffmpeg';
+import { send } from 'process';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+  // Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  // Error handling
+  if (!inputFilePath) {
+    res.status(400).send("Bad Request: Missing input file path.")
+  } else if (!outputFilePath) {
+    res.status(400).send("Bad Request: Missing output file path.")
+  }
+
+  // Converting video
+  Ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") //360p
+    .on("end", () => {
+      res.status(200).send("Video processed successfully.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`)
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000; //port could be undefined so we set port to 3000
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
